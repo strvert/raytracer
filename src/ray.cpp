1e40e00eeaffccda14b3619fea7498f2e962c799
strvworks <strv13570@gmail.com>
2020-06-04 03:46:17 +0900

    Tiny fix

--- src/ray.cpp
+++ src/ray.cpp
@@ -4,4 +4,5 @@ Color Ray::ToScreenLerpColor(Color Col1, Color Col2)
 {
     Vec3f UnitDir = Dir.Normalize();
     float T = 0.5 * (UnitDir.Y() + 1.0);
-    return (1.0 - T)*Col1 + T*Col2; }
+    return (1.0 - T)*Col1 + T*Col2;
+}
